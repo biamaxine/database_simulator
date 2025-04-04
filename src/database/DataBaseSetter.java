d5e8d5c2b808f685e64e4149db67c8da81dcd25b
biamaxine <2023005718@ifam.edu.br>
2025-04-04 13:03:48 -0400

    feat: CREATE annotation 'DataBaseGetter' feat: CREATE annotation 'DataBaseSetter'

---
+++ src/database/DataBaseSetter.java
@@ -0,0 +1,13 @@
+package database;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface DataBaseSetter {
+  String propertyName();
+  boolean uniqueKey();
+}
